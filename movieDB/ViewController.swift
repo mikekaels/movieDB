136cb6d36b1db6724691babcca2387f78fb95e52
kaelsihombing <sntmcl1@gmail.com>
2021-11-13 00:16:20 +0700

    Initial Commit

---
+++ movieDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movieDB
+//
+//  Created by Santo Michael Sihombing on 13/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
